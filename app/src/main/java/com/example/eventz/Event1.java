7a50bc42bd60c60f15424d033d47059d152c5223
Gabriel-Mihai Isleam <gabriel.isleam@gmail.com>
2020-04-08 21:32:00 +0300

    Update Event1.java

--- app/src/main/java/com/example/eventz/Event1.java
+++ app/src/main/java/com/example/eventz/Event1.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 // event 1
+// event 2 -------------------------------------------------------------
 public class Event1 extends AppCompatActivity {
 
     ImageButton heart;
