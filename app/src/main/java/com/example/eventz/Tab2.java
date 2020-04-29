2c9d339c41ec7945b3bb485221f1e4f002601834
gabriel-isleam <gabriel.isleam@gmail.com>
2020-04-29 20:41:21 +0300

    favourites added

--- app/src/main/java/com/example/eventz/Tab2.java
+++ app/src/main/java/com/example/eventz/Tab2.java
@@ -1,14 +1,35 @@
 package com.example.eventz;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 /**
@@ -24,6 +45,8 @@ public class Tab2 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FirebaseRecyclerOptions<Event> options;
+    private FirebaseRecyclerAdapter<Event, EventViewHolder> adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -65,8 +88,59 @@ public class Tab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        final View view = inflater.inflate(R.layout.fragment_tab, container, false);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.listView);
+        String userUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userUid).child("favourites");
+        options = new FirebaseRecyclerOptions.Builder<Event>().setQuery(databaseReference,Event.class).build();
+        adapter = new FirebaseRecyclerAdapter<Event, EventViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull EventViewHolder holder, final int position, @NonNull Event model) {
+                //Picasso.get().load(model.getImageUrl()).into(holder.imageViewIcon);
+                final String eventName = model.getName();
+                final Event event = model;
+                Glide.with(view.getContext()).load(model.getImageUrl()).override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).into(holder.imageViewIcon);
+                holder.textViewName.setText(model.getName());
+                holder.textViewLocation.setText(model.getLocation());
+                holder.imageViewIcon.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        /* retinem id key-ul evenimentului pe care dam click*/
+                        String event_key = getRef(position).getKey();
+                        /* intram in pagina evenimentului*/
+                        Intent event_page = new Intent(getActivity(), EventPage.class);
+                        event_page.putExtra("event_key", event_key);
+                        startActivity(event_page);
+                    }
+                });
+                holder.addToFavourites.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String event_key = getRef(position).getKey();
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        String userUid = user.getUid();
+                        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+                        dbRef.child("users").child(userUid).child("favourites").child(event_key).setValue(event);
+                        Toast.makeText(getContext(), "Event added to favourites!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_view_layout,parent,false);
+                return new EventViewHolder(view);
+            }
+        };
+
+        /* span count = nr. de evenimente pe linie */
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(view.getContext(),1);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,6 +165,8 @@ public class Tab2 extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+
+
     }
 
     /**
